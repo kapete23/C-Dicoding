bc20e7937d832fb862a835674f3875cd34b532c9
kapete23 <kevingokils97@gmail.com>
2022-06-07 01:17:14 +0700

    Menambahkan file 00 Hello World.c

---
+++ Source Code/00 Hello World.c
@@ -0,0 +1,14 @@
+/* File : 00 Hello World.c */
+/* Penulis : Kevin Petrus Tambunan, email: tambunankp23@gmail.com */
+/* Program menuliskan “Halo Dunia! ” dan "Hello World! " */
+
+#include<stdio.h>
+
+int
+main(void)
+{ /* Badan Program */
+    // Cetak "Halo Dunia!" dan “end-of-line” (ganti baris)
+    printf("Halo Dunia!\n   ");
+    // Cetak "Hello World!"
+    printf("Hello World!");
+}
